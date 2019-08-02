845f229edd68ccaddee0bdb3b17d96d7b40ad21e
kevchike <kevchike@gmail.com>
2019-08-02 18:07:58 +0100

    Auth set up

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiLoginRequest;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -34,6 +37,87 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest')->except(['logout', 'loggedInUser', 'apiLogout']);
+    }
+
+    /**
+     * Method created by me
+     * Get the needed authorization credentials from the request.
+     * User is allowed to use either username or email to login
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $field = filter_var($request->get($this->username()), FILTER_VALIDATE_EMAIL)
+                    ? $this->username() 
+                    : 'username';
+
+        return [
+            $field => $request->get($this->username()),
+            'password' => $request->password,
+        ];
+    }
+
+    /**
+     * Login user through the API (Create access token)
+     *
+     * @param  \App\Http\Requests\ApiLoginRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function apiLogin(ApiLoginRequest $request)
+    {
+        abort_if(!Auth::attempt($this->credentials($request)), 401);
+
+        $user = Auth::user();
+
+        $token = $user->createToken("LaraBlog");
+
+        $user['token'] = $token->accessToken;
+        $user['expires_at'] = $token->token->expires_at;
+        $user['token_type'] = 'Bearer';
+
+        array_except($user, ['id']);
+
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'title' => 'OK',
+            'message' => 'Login was successful.',
+            'data' => [$user]
+        ]);
+    }
+
+    /**
+     * Logout user through the API (Revoke the token)
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    public function apiLogout(Request $request)
+    {
+        // Revoke authenticated user's token.
+        $request->user()->token()->revoke();
+
+        // Delete the token from the database after revoking.
+        $request->user()->token()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'code' => 200,
+            'title' => 'OK',
+            'message' => 'Successfully logged out.'
+        ]);
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @return array
+     */
+    public function loggedInUser(Request $request)
+    {
+        return response()->json($request->user());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+Route::group(['prefix' => '/auth', 'namespace' => 'Auth'], function () {
+	Route::post('/login', 'LoginController@apiLogin');
+	Route::get('/logout', 'LoginController@apiLogout')->middleware('auth:api');
+	Route::get('/user', 'LoginController@loggedInUser')->middleware('auth:api');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
 });
