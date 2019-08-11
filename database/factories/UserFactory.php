30a83068ff90b624af2c967bb8a11e472450b954
kevchike <kevchike@gmail.com>
2019-08-11 13:20:52 +0100

    Bug fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 $factory->define(\App\User::class, function (Faker $faker) {
